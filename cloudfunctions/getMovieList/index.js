127ab61e0c67022b9f8d9fca474cdd11f0c35036
mokunshao <1627510957@qq.com>
2019-06-04 03:22:43 +0800

    完成首页

---
+++ cloudfunctions/getMovieList/index.js
@@ -0,0 +1,24 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+var rp = require('request-promise');
+
+cloud.init()
+
+
+// 云函数入口函数
+exports.main = async(event, context) => {
+  // const wxContext = cloud.getWXContext()
+  return rp(`https://api.douban.com/v2/movie/in_theaters?apikey=0df993c66c0c636e29ecbb5344252a4a&start=${event.start}&count=${event.count}`)
+    .then(function(res) {
+      return res;
+    })
+    .catch(function(err) {
+      return err;
+    });
+  // return {
+  //   event,
+  //   openid: wxContext.OPENID,
+  //   appid: wxContext.APPID,
+  //   unionid: wxContext.UNIONID,
+  // }
+}
