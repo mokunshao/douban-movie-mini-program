5fee19014aae1143b0c0ee0b6b7f53db3046a3b0
mokunshao <1627510957@qq.com>
2019-06-05 03:44:03 +0800

    删除无用代码

--- cloudfunctions/getDetail/index.js
+++ cloudfunctions/getDetail/index.js
@@ -6,13 +6,11 @@ cloud.init()
 
 // 云函数入口函数
 exports.main = async(event, context) => {
-  console.log(event.movieid)
   return rp(`https://api.douban.com/v2/movie/subject/${event.movieid}?apikey=0df993c66c0c636e29ecbb5344252a4a`)
-    .then(function(res) {
-      console.log(res)
+    .then((res)=> {
       return res
     })
-    .catch(function(err) {
+    .catch((err)=> {
       return err
     });
 }
--- cloudfunctions/getMovieList/index.js
+++ cloudfunctions/getMovieList/index.js
@@ -7,18 +7,11 @@ cloud.init()
 
 // 云函数入口函数
 exports.main = async(event, context) => {
-  // const wxContext = cloud.getWXContext()
   return rp(`https://api.douban.com/v2/movie/in_theaters?apikey=0df993c66c0c636e29ecbb5344252a4a&start=${event.start}&count=${event.count}`)
-    .then(function(res) {
+    .then((res)=> {
       return res;
     })
-    .catch(function(err) {
+    .catch((err)=> {
       return err;
     });
-  // return {
-  //   event,
-  //   openid: wxContext.OPENID,
-  //   appid: wxContext.APPID,
-  //   unionid: wxContext.UNIONID,
-  // }
 }
