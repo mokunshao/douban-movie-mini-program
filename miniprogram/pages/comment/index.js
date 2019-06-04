973c92266c3122553d7b4c9b7f73499ab35c4405
mokunshao <1627510957@qq.com>
2019-06-05 01:29:57 +0800

    完成评论功能

--- miniprogram/pages/comment/index.js
+++ miniprogram/pages/comment/index.js
@@ -1,17 +1,91 @@
 // pages/comment/index.js
+const db = wx.cloud.database();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    detail: {}
+    detail: {},
+    content: '',
+    rate: 5,
+    images: [],
+    fileIDs: [],
+    movieid: -1
   },
-
+  uploadImg() {
+    wx.chooseImage({
+      count: 9,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: (res) => {
+        const tempFilePaths = res.tempFilePaths
+        this.setData({
+          images: this.data.images.concat(tempFilePaths)
+        })
+      }
+    })
+  },
+  submitComment(event) {
+    wx.showLoading({
+      title: '评论中',
+    })
+    const promiseArr = []
+    for (let i = 0; i < this.data.images.length; i++) {
+      promiseArr.push(new Promise((resolve, reject) => {
+        const item = this.data.images[i]
+        const suffix = /\.\w+$/.exec(item)[0]
+        wx.cloud.uploadFile({
+          cloudPath: new Date().getTime() + suffix,
+          filePath: item,
+        }).then(res => {
+          this.setData({
+            fileIDs: this.data.fileIDs.concat(res.fileID)
+          })
+          resolve()
+        }).catch((e) => {
+          reject(e)
+        })
+      }))
+    }
+    Promise.all(promiseArr).then(() => {
+      db.collection('comment').add({
+        data: {
+          content: this.data.content,
+          rate: this.data.rate,
+          movieid: this.data.movieid,
+          fileIDs: this.data.fileIDs
+        }
+      }).then(() => {
+        wx.hideLoading()
+        wx.showToast({
+          title: '评价成功',
+        })
+      }).catch(() => {
+        wx.hideLoading()
+        wx.showToast({
+          title: '评价失败',
+        })
+      })
+    }).catch(e => console.log(e))
+  },
+  onCommentChange(event) {
+    this.setData({
+      content: event.detail
+    })
+  },
+  onRateChange(event) {
+    this.setData({
+      rate: event.detail
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    this.setData({
+      movieid: options.movieid
+    })
     wx.showLoading({
       title: '加载中',
     })
